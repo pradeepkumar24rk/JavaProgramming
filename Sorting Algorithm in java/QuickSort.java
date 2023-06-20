2eef11c431fd8a262b450dd67713786dab865a1b
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2023-06-20 06:12:23 +0530

    completed

--- Sorting Algorithm in java/QuickSort.java
+++ Sorting Algorithm in java/QuickSort.java
@@ -4,40 +4,44 @@
 public class QuickSort {
 
     public static void quicksort(int first, int last, int a[]) {
-        int i = first, pivot = first,
-                j = last;
+        if (first < last) {
 
-        while (i < j) {
-            while (a[i] <= a[pivot])
-                i++;
-            while (a[j] > a[pivot])
-                j--;
-            if (i > j)
-                // a[pivot] ^= a[j] ^=a[pivot] ^= a[j];
-            {   
-                int temp=a[pivot];
-                a[pivot]=a[j];
-                a[j]=temp;
-            }
-            else
+            int i = first, pivot = first,
+                    j = last;
+
+            while (i < j) {
+                while (a[i] <= a[pivot] && i < last)
+                    i++;
+                while (a[j] > a[pivot])
+                    j--;
+                if (i < j)
                 // a[i] ^= a[j] ^= a[i] ^= a[j];
-                {   
-                int temp=a[i];
-                a[i]=a[j];
-                a[j]=temp;
+                {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
             }
+
+            int temp = a[pivot];
+            a[pivot] = a[j];
+            a[j] = temp;
+            quicksort(first, j - 1, a);
+            quicksort(j + 1, last, a);
         }
-       
-        
 
     }
 
     public static void main(String[] args) {
-        int a[] = { 7, 6, 10, 5, 9,2, 1, 15, 7 };
+        int a[] = { 7, 6, 10, 5, 9, 2, 1, 15, 7 };
         quicksort(0, a.length - 1, a);
 
         for (int i : a) {
-            System.out.print(i+" ");
+            System.out.print(i + " ");
         }
     }
 }
+
+// BestCase averageCase worstCase
+// quick sort nlogn nlogn n^2 ----> it use partitioning method, it is less time
+// complexity compare to others sorting alogrithm
