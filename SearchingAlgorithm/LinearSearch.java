fc1f085bc3556276e1311f71f6d1e7265d94ca7f
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2023-06-25 20:38:46 +0530

    completed

---
+++ SearchingAlgorithm/LinearSearch.java
@@ -0,0 +1,17 @@
+package SearchingAlgorithm;
+
+public class LinearSearch {
+
+    static String linearSearch(int a[],int n,int t){
+        for(int i=0;i<n;i++)
+            if(a[i]==t)
+                return "Founded";
+        return "NotFounded";
+    }
+    public static void main(String[] args) {
+        int a[]={11,3,55,6,7,355,77};
+        int target=15;
+        int n=a.length;
+        System.out.println(linearSearch(a,n,target));
+    }
+}
