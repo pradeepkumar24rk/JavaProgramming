335e648785fa3bb142ecb248ae522369496a4968
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2023-06-27 11:05:25 +0530

    completed

--- TrainingProblems/CountingSubsequence.java
+++ TrainingProblems/CountingSubsequence.java
@@ -1,14 +1,14 @@
 public class CountingSubsequence {
     public static void main(String[] args) {
-        
-        int a[]={1,2,3,4} ,n=a.length;
-        System.out.println((1<<n)-1);  //total subsequence
-        int target=4,i=0;
-        while(target!=0){
-            if((target&1)==1)
-                System.out.print(a[i]+" ");
+
+        int a[] = { 1, 2, 3, 4 }, n = a.length;
+        System.out.println((1 << n) - 1); // total subsequence
+        int target = 4, i = 0;
+        while (target != 0) {
+            if ((target & 1) == 1)
+                System.out.print(a[i] + " ");
             i++;
-            target>>=1; 
+            target >>= 1;
         }
     }
 }
--- TrainingProblems/LargestSumOfSubsequence.java
+++ TrainingProblems/LargestSumOfSubsequence.java
@@ -1,22 +1,22 @@
 public class LargestSumOfSubsequence {
     public static void main(String[] args) {
-        int a[]={1,2,3,4,5},n=5,count=(1<<n)-1;
-        int max=Integer.MIN_VALUE;
-        for(int i=1;i<=count;i++){
-            int target=i,sum=0,j=0;
-            while(target!=0){
-                if((target&1)==1){
-                    sum+=a[j];
-                    System.out.print(a[j]+" ");
+        int a[] = { 1, 2, 3, 4, 5 }, n = 5, count = (1 << n) - 1;
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i <= count; i++) {
+            int target = i, sum = 0, j = 0;
+            while (target != 0) {
+                if ((target & 1) == 1) {
+                    sum += a[j];
+                    System.out.print(a[j] + " ");
                 }
                 j++;
-                target>>=1;
+                target >>= 1;
             }
             System.out.println();
-            System.out.println("sum of array :"+sum);
-            if(max<sum)
-                max=sum;
+            System.out.println("sum of array :" + sum);
+            if (max < sum)
+                max = sum;
         }
-        System.out.println("largest Sum Of Subsequence :"+max);
+        System.out.println("largest Sum Of Subsequence :" + max);
     }
 }
