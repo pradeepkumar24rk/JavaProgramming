80c197b203b3743c7d01d3ef9d4a7f12f17c21e0
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2023-07-16 11:48:12 +0530

    modify

---
+++ TrainingProblems/demo.java
@@ -0,0 +1,17 @@
+public class demo {
+    static int a=10;
+    
+    static int func(){
+    int b=a+1;
+    a=a+1;
+        return b;
+    }
+    public static void main(String[] args) {
+        
+        System.out.println(func());
+        a=a+1;
+        System.out.println(a);
+        
+        
+    }
+}
