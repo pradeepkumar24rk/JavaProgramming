ff538f4a487a08a210f37edd5cb7b1d6ea5a7794
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2024-01-04 06:41:16 +0530

    calendar program is completed

--- ApplicationBasedProgram/Calendar.java
+++ ApplicationBasedProgram/Calendar.java
@@ -1,7 +1,71 @@
 public class Calendar {
-    public static void main(String[] args) {
-        
-    }
+
+        public static boolean Leap(int n) {
+                if ((n % 4 == 0 && n % 100 != 0) || (n % 400 == 0))
+                        return true;
+                return false;
+        }
+
+        public static int tempDate(int month, int year) {
+                if (month == 4 || month == 6 || month == 8 || month == 10 || month == 12)
+                        return month;
+                else if (month == 1)
+                        return Leap(year) ? 4 : 3;
+                else if (month == 3)
+                        return 7;
+                else if (month == 9 || month == 5)
+                        return month == 9 ? 5 : 9;
+                else if (month == 11 || month == 7)
+                        return month == 7 ? 11 : 7;
+                return 0;
+        }
+
+        public static int yearOldDays(int year) {
+                int tensYear = year % 100;
+                year -= tensYear;
+                int sum = 0;
+                int yearOldays[] = { 5, 3, 1, 0 };
+                year = (year % 400) / 100;
+                // System.out.println(year);
+                if(year!=0)
+                        sum += yearOldays[year - 1];
+                // System.out.println(sum);
+                int leapYearInTensYear = (int) tensYear / 4;
+                int nonleapYearInTensYear = tensYear - leapYearInTensYear;
+                // System.out.println(((leapYearInTensYear * 2) + nonleapYearInTensYear)%7);
+                sum += ((leapYearInTensYear * 2) + nonleapYearInTensYear) % 7;
+                // System.out.println(sum);
+                return sum;
+
+        }
+
+        public static int findFebLastDay(int year) {
+                int sum = Leap(year) ? 4 : 3;
+                // System.out.println(sum+yearOldDays(year-1));
+                return sum + yearOldDays(year-1);
+        }
+
+        public static void main(String[] args) {
+                String day[] = { "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday" };
+                int date = 24, month = 04, year = 2002;
+                int febLastDay = findFebLastDay(year);
+                int tempDate = tempDate(month, year), tempDay = febLastDay;
+                if (tempDate == date)
+                        System.out.println(day[tempDay]);
+                else {
+                        int dayDifference = date - tempDate;
+                        if(date>tempDate) {
+                                tempDay  = (tempDay+dayDifference) % 7;
+                        } else {
+                                tempDay = tempDay+dayDifference;
+                                if(tempDay<0) {
+                                        tempDay+=7;
+                                }
+                        }
+                        // System.out.println(tempDay);
+                        System.out.println(day[tempDay]);
+                }
+        }
 }
 
 
@@ -31,8 +95,9 @@ years  no.of odd days
 300 - 1 days
 400 - 0 days
 
-Feb last day = (date/month) ==> 4/4, 6/6, 8/8,10/10,
-                                12/12,9/5,11/7,7/11,7/3
+Feb last day = (date/month) ==> 4/4, 6/6, 8/8,10/10,12/12,
+                                9/5,5/9,11/7,7/11,
+                                7/3
                                 jan - 3(nonleap) and 4(leap)
                                 
 eg: if feb 28 - friday
