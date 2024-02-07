332443e8f8c0bb48d1aa8b3ef4e7a705cd1017f1
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2024-02-07 11:44:28 +0530

    Completed

---
+++ ApplicationBasedProgram/ChessQueenMoves.java
@@ -0,0 +1,40 @@
+/**
+ * ChessQueenMoves
+ */
+// import java.util.*;
+public class ChessQueenMoves {
+
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        int row = 8, col = 8;
+        int sourceRow = 2, sourceCol = 1;
+        int board[][] = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == sourceRow && j == sourceCol)
+                    board[i][j] = 1;
+                else
+                    board[i][j] = 0;
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        int drow = 1, dcol = 0;
+        if (drow == sourceRow || sourceCol == dcol || drow + dcol == sourceRow + sourceCol
+                || drow - dcol == sourceRow - sourceCol) {
+            System.out.println("valid");
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (i == drow && j == dcol)
+                        board[i][j] = 1;
+                    else
+                        board[i][j] = 0;
+                    System.out.print(board[i][j] + " ");
+                }
+                System.out.println();
+            }
+        } else
+            System.out.println("not valid");
+    }
+}
