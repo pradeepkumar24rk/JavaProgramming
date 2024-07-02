f1e082634723568fff7b9c08aaa41286a716832f
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2024-07-02 17:17:57 +0530

    Simplified code function is added

--- NumberBasedProblems/TailorZero.java
+++ NumberBasedProblems/TailorZero.java
@@ -1,11 +1,28 @@
 package NumberBasedProblems;
 
 public class TailorZero {
+
+    static int findTrailingZeros(int n)
+    {
+        if (n < 0) // Negative Number Edge Case
+            return -1;
+ 
+        // Initialize result
+        int count = 0;
+ 
+        // Keep dividing n by powers
+        // of 5 and update count
+        for (int i = 5; n / i >= 1; i *= 5)
+            count += n / i;
+ 
+        return count;
+    }
+    
     public static void main(String[] args) {
         long n=201,sum=1,count=0;
         for(long i=2;i<=n;i++)
             sum*=i;
-        System.out.println(sum);
+        System.out.println("Factorial = "+sum);
         while(sum!=0){
             long rem=sum%10;
             if(rem!=0)
@@ -13,7 +30,7 @@ public class TailorZero {
             count++;
             sum/=10;
         }
-        System.out.println(count);
+        System.out.println("trailing zero count = "+count);
     }
 }
  
